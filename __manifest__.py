9956a6358207af33354d868d3f72aa467c2d2d10
Mila <martins.fm28@outlook.com>
2023-04-11 13:20:42 -0300

    Função para recomendação de livros criada

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     'data': [
         'security/ir.model.access.csv',
         'wizard/book_registry_view.xml',
-        'wizard/update_quantity_view.xml',
+        'wizard/recommender_view.xml',
         'views/publisher_view.xml',
         'views/author_view.xml',
         'views/genre_view.xml',
@@ -25,6 +25,7 @@
         'data/authors.xml',
         'data/publishers.xml',
         'data/customers.xml',
+        'data/books.xml',
     ],
     'application': True,
     'license': 'LGPL-3',
