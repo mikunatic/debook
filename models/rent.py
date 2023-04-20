0a2826fafab49a2571b7b0f0a36b9a6b488776c7
Mila <martins.fm28@outlook.com>
2023-04-20 16:55:20 -0300

    Adicionando nova lógica para o aluguel de livros

--- models/rent.py
+++ models/rent.py
@@ -1,5 +1,4 @@
 from odoo import fields, models, api
-from datetime import date
 import datetime
 
 
@@ -10,13 +9,13 @@ class Rent(models.Model):
     book_id = fields.Many2one('book', string="Livro", domain="[('rent_id','=',False)]")
     expire_selection = fields.Selection([('fortnight', '15 dias'),
                                          ('one_month', '1 mês'),
-                                         ('two_months','2 meses')])
+                                         ('two_months','2 meses')], string="Seleção de Vencimento")
     expire_date = fields.Datetime()
     date_time_fixed = fields.Datetime(string="Data de Vencimento")
     state = fields.Selection([('rented','Alugado'),
                               ('pending','Devolução Pendente'),
-                              ('expired','Devolvido'),
-                              ], default='rented')
+                              ('returned','Devolvido'),
+                              ], compute="expire")
 
     # fazer função que calcula a data de vencimento de acordo com o selection
     @api.onchange('expire_selection')
@@ -39,4 +38,23 @@ class Rent(models.Model):
                 rec.expire_date = date
             rec.date_time_fixed = rec.expire_date + datetime.timedelta(hours=3)
 
-    # fazer função que envia email para o customer quando o aluguel for vencido
+    @api.model
+    def create(self, vals_list):
+        vals_list['state'] = 'rented'
+        return super(Rent, self).create(vals_list)
+
+    @api.depends('expire_date')
+    def expire(self):
+        for rec in self:
+            now = datetime.datetime.now()
+            expire_date = rec.expire_date
+            if now > expire_date:
+                rec.state = 'pending'
+            elif rec.state == 'returned':
+                break
+            elif now < expire_date:
+                rec.state = 'rented'
+
+    def return_book(self):
+        for rec in self:
+            rec.state = 'returned'
