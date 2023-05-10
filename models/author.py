ceccbc3993a4867829206ea99318f80d8010c785
Mila <martins.fm28@outlook.com>
2023-05-10 16:14:03 -0300

    Mudança de lógica para popular tabela de livros dos autores e das editoras de forma mais simples

--- models/author.py
+++ models/author.py
@@ -5,9 +5,4 @@ class Author(models.Model):
     _name = 'author'
 
     name = fields.Char("Autor", required=True)
-    book_ids = fields.Many2many('book', compute="populate_books", string="Livros")
-
-    def populate_books(self):
-        for rec in self:
-            book_ids = self.env['book'].search([('author_id','=',rec.id)])
-            rec.book_ids = book_ids.ids
+    book_ids = fields.One2many(comodel_name="book", inverse_name="author_id", string="Livros")
--- models/publisher.py
+++ models/publisher.py
@@ -5,9 +5,4 @@ class Publisher(models.Model):
     _name='publisher'
 
     name = fields.Char("Editora", required=True)
-    book_ids = fields.Many2many('book', compute="populate_books", string="Livros")
-
-    def populate_books(self):
-        for rec in self:
-            book_ids = self.env['book'].search([('publisher_id','=',rec.id)])
-            rec.book_ids = book_ids.ids
+    book_ids = fields.One2many(comodel_name="book", inverse_name="publisher_id", string="Livros")
